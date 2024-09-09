8c3e5c4c2868f6a49a365d1f015d9382729c1ab6
DarshanG2024 <darshan.gadakh@thinkitive.com>
2024-09-09 20:37:04 +0530

    feat: day5 reactJS

---
+++ day-5/destructuring.js
@@ -0,0 +1,28 @@
+// const user= [{
+//     name:"A",
+//     age:12
+// },
+
+// {
+//     name:"B",
+//     age:13
+// },
+// {
+//     name:"C",
+//     age:13
+// },
+// {
+//     name:"D",
+//     age:14
+// },
+// ]
+
+const {name: userName, ages}= {
+    name:"A",
+    age:12
+}
+
+
+console.log(userName)
+
+
---
+++ day-5/spreadOperator.js
@@ -0,0 +1,8 @@
+let hobbies=["playing", "working"];
+
+let newhobbies=["watching"];
+
+//spread operator (...)
+
+let mergedHobbies= [...hobbies, ...newhobbies];
+console.log(mergedHobbies);
