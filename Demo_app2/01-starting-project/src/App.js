30b1e7c160d500266290767225998ec167fc4255
DarshanG2024 <darshan.gadakh@thinkitive.com>
2024-10-03 20:59:07 +0530

    Day 17 training reactjs form validation

---
+++ Demo_app2/01-starting-project/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css'; 
+import FormValidation from './components/FormValidation/FormValidation';
+
+function App() {
+  return (
+    <div>
+      <FormValidation></FormValidation>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ Demo_app2/01-starting-project/src/index.js
@@ -0,0 +1,21 @@
+// import React from 'react';
+// import { createRoot } from 'react-dom/client';
+// import App from './App';
+
+// const rootElement = document.getElementById('root');
+// const root = createRoot(rootElement);
+// root.render(<App />);
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App'; 
+import './index.css'; 
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
